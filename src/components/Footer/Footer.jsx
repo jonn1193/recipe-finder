706b62ab19099df895cb9d53c1125138b5719bcd
jonn1193 <jayc223@yahoo.com>
2026-09-14 19:31:01 -0700

    footer udpate

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copyright">&copy; 2026 Recipe Finder</p>
-      <p className="footer__note">Created by Jon N. Powered by TheMealDB.</p>
+      <p className="footer__note">Created by Jon N. | Powered by TheMealDB.</p>
     </footer>
   );
 }
